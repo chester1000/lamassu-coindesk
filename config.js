69caf404e7186d9b6e31edbc5d87107900e02847
Damian Mee <damian.wasilewski@gmail.com>
2014-08-18 01:59:45 +0200

    feat(structure): plugin structure changed to fit with new API

---
+++ config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var _ = require('lodash');
+
+
+exports.NAME = 'Coindesk';
+exports.SUPPORTED_MODULES = ['ticker'];
+
+// list of currencies updated on 2014-08-17 00:35
+//    from http://api.coindesk.com/v1/bpi/supported-currencies.json
+exports.SUPPORTED_CURRENCIES = [
+  'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM',
+  'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTC',
+  'BTN', 'BWP', 'BYR', 'BZD', 'CAD', 'CDF', 'CHF', 'CLF', 'CLP', 'CNY', 'COP',
+  'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EEK', 'EGP', 'ERN',
+  'ETB', 'EUR', 'FJD', 'FKP', 'GBP', 'GEL', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ',
+  'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'INR', 'IQD', 'IRR',
+  'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KMF', 'KPW', 'KRW',
+  'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LTL', 'LVL', 'LYD',
+  'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRO', 'MTL', 'MUR', 'MVR',
+  'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR',
+  'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB',
+  'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLL', 'SOS', 'SRD',
+  'STD', 'SVC', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD',
+  'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VEF', 'VND', 'VUV', 'WST',
+  'XAF', 'XAG', 'XAU', 'XBT', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMK',
+  'ZMW', 'ZWL'];
+
+exports.API_ENDPOINT = 'https://api.coindesk.com/v1/bpi/';
+
+
+exports.SATOSHI_FACTOR = 1e8;
+exports.FUDGE_FACTOR = NaN;
+
+exports.config = function config(config) {
+  if (config) _.merge(exports, config, true);
+};
+
+exports.supports = function supports(moduleName) {
+  return exports.SUPPORTED_MODULES.indexOf(moduleName) !== -1;
+};
